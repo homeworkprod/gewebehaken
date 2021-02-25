e76852d54a172620590f489efc5c502aae5973d1
Jochen Kupperschmidt <homework@nwsnet.de>
2021-02-25 23:53:09 +0100

    Use `json` argument of Werkzeug's test client

--- tests/base.py
+++ tests/base.py
@@ -1,5 +1,3 @@
-import json
-
 import pytest
 
 from gewebehaken.app import create_app
@@ -7,9 +5,4 @@ from gewebehaken.app import create_app
 
 def post_json(client, path, data, headers=None):
     """Send a POST request with a JSON body."""
-    return client.post(
-        path,
-        content_type='application/json',
-        data=json.dumps(data),
-        headers=headers,
-    )
+    return client.post(path, json=data, headers=headers)
